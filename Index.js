47466a5d569507a22766ed496a4ce75873b0d4c8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 16:06:24 +0000

    c35

--- Index.js
+++ Index.js
@@ -7,44 +7,47 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 client.on('ready', () => console.log('The Bot is ready!'));
 let keyfs = Math.ceil(Math.random()*10000);
 console.log(keyfs);
-client.on('message', (msg) => {
+client.on('messageCreate', (msg) => {
+  if(!msg.author.bot) {
     console.log("Receive: "+msg.content)
     let message = msg.content.toLowerCase();
-      if (message === '!test') {
+    var args = msg.content.split(" ");
+    var command = args[0].toLowerCase();
+      if (command === '!test') {
        msg.channel.send(`testing, active`);
        console.log("Sent: testing, active");
       };
-      if (message === '!crash_'+keyfs) {
+      if (command === '!crash_'+keyfs) {
         msg.channel.send(`Crashing`);
         console.log("Sent: Crashing");
       };
-      if (message === '!game') {
+      if (command === '!game') {
         msg.channel.send(`***Game***
         **Game:** *https://www.roblox.com/games/8927096857*
         *Remmember to join the group*
         **Group:** *https://www.roblox.com/groups/8517669*`);
         console.log("Sent: GameUrls");
        };
-       let messaget = message.substring(16);
-       let args = message.split(" ")
-       if (messaget === "!purge") {
+       if (command === "!purge") {
 
-        var amount = parseInt(args[0])
+        var amount = parseInt(args[1])
 
-        if (!amount) return message.channel.send("Please specify the amount of messages you want me to delete")
-        if (amount > 100 || amount < 1) return message.channel.send("Please select a number *between* 100 and 1")
+        if (!amount) return msg.channel.send("Please specify the amount of messages you want me to delete")
+        if (amount > 100 || amount < 1) return msg.channel.send("Please select a number *between* 100 and 1")
+        msg.delete()
+        msg.channel.bulkDelete(amount).catch(err => {
+              msg.channel.send(':x: Due to Discord Limitations, I cannot delete messages older than 14 days') })
 
-        message.channel.bulkDelete(amount).catch(err => {
-              message.channel.send(':x: Due to Discord Limitations, I cannot delete messages older than 14 days') })
-
-        let delmessage = message.channel.send(`Deleted \`${amount}\` messages`)
+        let delmessage = msg.channel.send(`Deleted \`${amount}\` ***messages*** *(You may delete this message)*`)
+        /* Checking needed
         setTimeout(() => {
           delmessage.delete()
         }, 2000)
-
+        */
        }
+      }
 });
-client.login(process.env.BOT_TOKEN);
+client.login(/*process.env.BOT_TOKEN*/"OTQ3MzE5MzM5MDE3NzI4MDQw.Yhrh4g.wbqd1jqgjQTgyI2_p2ZBilAkzcM");
 
 /*
 SC:
