7d59a5cacc74871c3b4a4b1df5e75377aea5bab1
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 04:07:50 +0000

    updated

--- Index.js
+++ Index.js
@@ -10,13 +10,28 @@ console.log(keyfs);
 client.on('message', (msg) => {
     console.log("Receive: "+msg.content)
     let message = msg.content.toLowerCase();
-  if (message === '!test') {
-    msg.channel.send(`testing, active`);
-    console.log("Sent: testing, active");
-  };
-  if (message === '!crash_'+keyfs) {
-    msg.channel.send(`Crashing`);
-    console.log("Sent: Crashing");
-  };
+      if (message === '!test') {
+       msg.channel.send(`testing, active`);
+       console.log("Sent: testing, active");
+      };
+      if (message === '!crash_'+keyfs) {
+        msg.channel.send(`Crashing`);
+        console.log("Sent: Crashing");
+      };
+      if (message === '!game') {
+        msg.channel.send(`***Game***
+        **Game:** *https://www.roblox.com/games/8927096857*
+        *Remmember to join the group*
+        **Group:** *https://www.roblox.com/groups/8517669*`);
+        console.log("Sent: GameUrls");
+       };
 });
 client.login(process.env.BOT_TOKEN);
+
+/*
+SC:
+y2ivK1kvSyK-UTOLPQxEJoP4-yRGAsCn
+TT
+OTE1MzUwMzEwMzcxODcyODYw.YaaUaA.gfM3tlRc3Hc4JhiSpkQukzwj5Ec
+
+*/
