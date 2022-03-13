a6968f002d56815f44adb7993167f9f668615d84
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 01:19:48 +0000

    c6

--- Index.js
+++ Index.js
@@ -1,23 +1,22 @@
-console.log("STARTED")
+console.log("STARTED");
 const Discord = require('discord.js');
-console.log("LOAD DOTENV")
+console.log("LOAD DOTENV");
 require('dotenv').config();
-console.log("LOADED DOTENV")
+console.log("LOADED DOTENV");
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 client.on('ready', () => console.log('The Bot is ready!'));
-let keyfs = Math.ceil(Math.random()*10000)
-console.log(keyfs)
+let keyfs = Math.ceil(Math.random()*10000);
+console.log(keyfs);
 client.on('message', (msg) => {
     console.log("Receive: "+msg.content)
     let message = msg.content.toLowerCase();
   if (message === '!test') {
     msg.channel.send(`testing, active`);
-    console.log("Sent: testing, active")
-  }
+    console.log("Sent: testing, active");
+  };
   if (message === '!crash_'+keyfs) {
     msg.channel.send(`Crashing`);
-    console.log("Sent: Crashing")
-    lol
-  }
+    console.log("Sent: Crashing");
+  };
 });
