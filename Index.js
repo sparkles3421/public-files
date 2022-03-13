8a3ae3ae5908f037962b270478a41872d1ee7981
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 14:20:01 +0000

    c30

--- Index.js
+++ Index.js
@@ -25,6 +25,24 @@ client.on('message', (msg) => {
         **Group:** *https://www.roblox.com/groups/8517669*`);
         console.log("Sent: GameUrls");
        };
+       let messaget = message.substring(16);
+       let args = message.split(" ")
+       if (messaget === "!purge") {
+
+        var amount = parseInt(args[0])
+
+        if (!amount) return message.channel.send("Please specify the amount of messages you want me to delete")
+        if (amount > 100 || amount < 1) return message.channel.send("Please select a number *between* 100 and 1")
+
+        message.channel.bulkDelete(amount).catch(err => {
+              message.channel.send(':x: Due to Discord Limitations, I cannot delete messages older than 14 days') })
+
+        let msg = await message.channel.send(`Deleted \`${amount}\` messages`)
+        setTimeout(() => {
+            msg.delete()
+        }, 2000)
+
+       }
 });
 client.login(process.env.BOT_TOKEN);
 
