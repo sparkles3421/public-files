2643ada1338e5b1890a0ee63aff288667c4544c6
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 22:14:12 +0000

    c47

--- Index.js
+++ Index.js
@@ -31,9 +31,10 @@ client.on('messageCreate', async (msg) => {
         console.log("Sent: GameUrls");
        };
        if (command === "!purge") {
+        member.roles.cache.has('947318530351706112');
+        if (member.roles.cache.some(role => role.name === 'BotAdmin')) {
         console.log("purge: "+args[1])
         var amount = parseInt(args[1])
-
         if (!amount) return msg.channel.send("Please specify the amount of messages you want me to delete")
         if (amount > 100 || amount < 1) return msg.channel.send("Please select a number *between* 100 and 1")
         msg.delete()
@@ -46,6 +47,7 @@ client.on('messageCreate', async (msg) => {
           delmessage.delete()
         }, 2000)
         */
+      }
        }
        if (command === 'hello') {
         msg.channel.send(`Hello, How are you?`);
@@ -53,11 +55,18 @@ client.on('messageCreate', async (msg) => {
         msg.react('🙁');
         console.log("Sent: hello");
        }
+       if (command === '!ping') {  
+        msg.channel.send(`🏓reaction is ${Date.now() - message.createdTimestamp}ms. API is ${Math.round(client.ws.ping)}ms. 🏓`);
+      }
        if (command === '!help') {
         msg.channel.send({
           embeds: [{
             title: 'help',
-            description: 'help commands',
+            description: 'help commands'+`
+            !help - all commands
+            !game - shows game url
+            !test - checks bot servers
+            !ping - current ping`,
             image: {url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F46%2FQuestion_mark_%2528black%2529.svg%2F1200px-Question_mark_%2528black%2529.svg.png&imgrefurl=https%3A%2F%2Fen.wiktionary.org%2Fwiki%2Fquestion_mark&tbnid=YCWjl8F6gBqD0M&vet=12ahUKEwiPjd7jhcT2AhUP_hoKHZW7CGMQMygAegUIARDUAQ..i&docid=knXL1q1XHbToQM&w=1200&h=1200&q=questionmark%20image&ved=2ahUKEwiPjd7jhcT2AhUP_hoKHZW7CGMQMygAegUIARDUAQ'}
           }]
         })
